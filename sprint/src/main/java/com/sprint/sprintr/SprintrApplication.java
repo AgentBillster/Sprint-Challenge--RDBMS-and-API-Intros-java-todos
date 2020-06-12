c817315290fdb8e16de9738505b5dd9b3e1b51f5
william <william@williams-Air.attlocal.net>
2020-06-12 11:28:48 -0500

    nope

---
+++ sprint/src/main/java/com/sprint/sprintr/SprintrApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.sprintr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintrApplication.class, args);
+    }
+
+}
---
+++ sprint/src/test/java/com/sprint/sprintr/SprintrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.sprintr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
